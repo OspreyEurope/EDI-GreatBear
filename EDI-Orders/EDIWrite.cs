1b1182a2f17a46c311eecc094c1948871113cf6a
Marc Heidemann <marc.heidemann@ospreyeurope.com>
2023-01-24 11:25:47 +0000

    Added additional commenting and depth to the commenting, this should help to make the process maintainable for others.

--- EDI-Orders/EDIWrite.cs
+++ EDI-Orders/EDIWrite.cs
@@ -13,11 +13,20 @@ namespace EDI_Orders
     public class EDIWrite
     {
         #region Writes EDI File in Fact format
+        /**
+         * This function writes the responce file, 
+         * this can will be changed based on the file that needs to be produced,
+         * this can be used as a basis but is currently set up to produce an edi
+         * responce simialr to the EDIFact file that is being recieved.
+         */
         public static void WriteEDIFact (SqlConnection con)
         {
             StreamWriter streamWriter = new StreamWriter("OSP_EDIWriteFile");
             streamWriter.Write("UNA:+.?'");
             int counter = 0;
+            /**
+             * Retrives the data from the database and then writes it line by line into a file.
+             */
             DataTable data = SharedFunctions.GetData(con,"OSP_Get_Responce_Data");
             for (int i = 0; i < data.Rows.Count; i++)
             {
@@ -28,6 +37,9 @@ namespace EDI_Orders
                              .ToArray();
                 string header = Lookups.WriteLookUp(nameArray[i]);
                 string text = "";
+                /**
+                 * This sectin writes all the information in that data row into a single line in the EDI file.
+                 */
                 for (int j = 0; j < data.Columns.Count; j++)
                 {
                     text = text + row[j].ToString();
