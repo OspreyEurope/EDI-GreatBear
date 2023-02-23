bc9ad8892c5fdc8c12b1579d855b65242ac705f3
Marc Heidemann <marc.heidemann@ospreyeurope.com>
2023-02-23 11:50:36 +0000

    Fixed alignments.

--- EDI-Orders/Program.cs
+++ EDI-Orders/Program.cs
@@ -436,20 +436,20 @@ namespace EDI_Orders
                     #endregion
                     #region KTN Read
                     case "KTN":
-                        //KTN.ProcessKTN("C://Bespoke/EDI/KTNSamples/STKADJ00000005.txt", conDev);           //Seems to work
-                        //KTN.ProcessKTN("C://Bespoke/EDI/KTNSamples/STKADJ00000008.txt", conDev);           //Seems to work
-                        //KTN.ProcessKTN("C://Bespoke/EDI/KTNSamples/STKADJ00000009.txt", conDev);           //Seems to work
-                        //KTN.ProcessKTN("C://Bespoke/EDI/KTNSamples/STKADJ00000010.txt", conDev);           //Seems to work
-                        //KTN.ProcessKTN("C://Bespoke/EDI/KTNSamples/STKADJ00000011.txt", conDev);           //Seems to work
-                        
+                        //KTN.ProcessKTN("C://Bespoke/EDI/KTNSamples/STKADJ00000005.txt", conDev);         //Seems to work
+                        //KTN.ProcessKTN("C://Bespoke/EDI/KTNSamples/STKADJ00000008.txt", conDev);         //Seems to work
+                        //KTN.ProcessKTN("C://Bespoke/EDI/KTNSamples/STKADJ00000009.txt", conDev);         //Seems to work
+                        //KTN.ProcessKTN("C://Bespoke/EDI/KTNSamples/STKADJ00000010.txt", conDev);         //Seems to work
+                        //KTN.ProcessKTN("C://Bespoke/EDI/KTNSamples/STKADJ00000011.txt", conDev);         //Seems to work
+
                         //KTN.ProcessKTN("C://Bespoke/EDI/KTNSamples/RECCON00000003.txt", conDev);         //Seems to work
                         //KTN.ProcessKTN("C://Bespoke/EDI/KTNSamples/RECCON00000005.txt", conDev);         //Seems to work
                         //KTN.ProcessKTN("C://Bespoke/EDI/KTNSamples/RECCON00000006.txt", conDev);         //Seems to work
-                        
-                        //KTN.ProcessKTN("C://Bespoke/EDI/KTNSamples/PPLCON00000018.txt", conDev);          //To be done
-                        //KTN.ProcessKTN("C://Bespoke/EDI/KTNSamples/PPLCON00000017.txt", conDev);           //To be done
-                        //KTN.ProcessKTN("C://Bespoke/EDI/KTNSamples/PPLCON00000012.txt", conDev);        //To be done
-                        KTN.ProcessKTN("C://Bespoke/EDI/KTNSamples/PPLCON00000011.txt", conDev);          //To be done
+
+                        //KTN.ProcessKTN("C://Bespoke/EDI/KTNSamples/PPLCON00000018.txt", conDev);         //Seems to work
+                        //KTN.ProcessKTN("C://Bespoke/EDI/KTNSamples/PPLCON00000017.txt", conDev);         //Seems to work
+                        //KTN.ProcessKTN("C://Bespoke/EDI/KTNSamples/PPLCON00000012.txt", conDev);         //Seems to work
+                        KTN.ProcessKTN("C://Bespoke/EDI/KTNSamples/PPLCON00000011.txt", conDev);           //Seems to work
                         break;
                     #endregion
                     default:
