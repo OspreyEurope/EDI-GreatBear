05f3434e938429e806f5e3179a25725c2c913a43
Marc Heidemann <mheidemann@helenoftroy.com>
2023-04-27 16:37:00 +0100

    email updates

--- EDI-Orders/SharedFunctions.cs
+++ EDI-Orders/SharedFunctions.cs
@@ -499,9 +499,9 @@ namespace EDI_Orders
         {
             using (var mail = new MailMessage())
             {
-                //mail.To.Add(ConfigurationManager.AppSettings["AlertEmail"]);
-                //mail.To.Add(ConfigurationManager.AppSettings["AlertEmail2"]);
-                mail.To.Add(ConfigurationManager.AppSettings["AlertEmail3"]);
+                mail.To.Add(ConfigurationManager.AppSettings["AlertEmail"]);
+                mail.To.Add(ConfigurationManager.AppSettings["AlertEmail2"]);
+                //mail.To.Add(ConfigurationManager.AppSettings["AlertEmail3"]);
                 mail.From = new MailAddress(ConfigurationManager.AppSettings["FromAddress"]);
                 mail.Subject = "EDI handling program:";
                 mail.Body = DateTime.Now + " Error in " + v + ": "+ ex.Message;
