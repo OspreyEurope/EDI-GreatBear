35ae7e94e821dd0c35e5894a9337ce0735d373c1
marc.heidemann@ospreyeurope.com <marc.heidemann@ospreyeurope.com>
2023-08-03 10:11:40 +0100

    Change to delcity for incoterms in ktn order

--- EDI-Orders/EDIWrite.cs
+++ EDI-Orders/EDIWrite.cs
@@ -335,7 +335,8 @@ namespace EDI_Orders
 
                     text = row["Incoterms"].ToString();                                //Section reserved for incoterms
                     text = text.PadRight(10, ' ');
-                    text = text + row["DelCity"].ToString().PadRight(25, ' ');
+                    string temp = row["DelCity"].ToString().Substring(0, 25);
+                    text = text + temp.PadRight(25, ' ');
                     text = text + row["DelCountryCode"].ToString().PadRight(3, ' ');
                     text = text.PadLeft(175, ' ');
 
