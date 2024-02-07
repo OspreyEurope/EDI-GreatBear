2c627b58482e9b1ee95b6ddd960fd9e2bfd4b8ed
OspreyEurope <marc.heidemann@ospreyeurope.com>
2024-02-07 07:58:51 +0000

    Added per line back in and fixed spelling error in addressline2

--- EDI-Orders/EDIWrite.cs
+++ EDI-Orders/EDIWrite.cs
@@ -888,10 +888,10 @@ namespace EDI_Orders
                         DataRow GDPR = GDPRData.Rows[0];
                         streamWriter.Write("N1*BP*" + GDPR["PostalName"] + "*91*" + row["CustomerAccountRef"] + "~");
                         streamWriter.Write("N1*BT*" + GDPR["PostalName"] + "*91*" + row["CustomerAccountRef"] + "~");
-                        //streamWriter.Write("N3*" + GDPR["AddressLine1"] + "*" + GDPR["AddressLine2"] + "~");
+                        streamWriter.Write("N3*" + GDPR["AddressLine1"] + "*" + GDPR["AddressLine2"] + "~");
                         streamWriter.Write("N3*" + GDPR["AddressLine1"]);      
                         
-                        if ((GDPR["AddressLie2"] != null) || !(GDPR["AddressLie2"].Equals("")))
+                        if (GDPR["AddressLine2"] != null)
                         {
                             streamWriter.Write("*" + GDPR["AddressLine2"]);
                         }
@@ -900,10 +900,10 @@ namespace EDI_Orders
                         
                         streamWriter.Write("N4*" + row["DelCity"] + "**" + row["DelPostCode"] + "*" + row["DelCountryCode"] + "~");
                         streamWriter.Write("N1*ST*" + GDPR["PostalName"] + "*91*" + row["CustomerAccountRef"] + "-" + row["CustomerAccountRef"] + "~");
-                        //streamWriter.Write("N3*" + GDPR["AddressLine1"] + "*" + GDPR["AddressLine2"] + "~"); 
+                        streamWriter.Write("N3*" + GDPR["AddressLine1"] + "*" + GDPR["AddressLine2"] + "~"); 
                         streamWriter.Write("N3*" + GDPR["AddressLine1"]);
 
-                        if ((GDPR["AddressLie2"] != null) || !(GDPR["AddressLie2"].Equals("")))
+                        if (GDPR["AddressLine2"] != null)
                         {
                             streamWriter.Write("*" + GDPR["AddressLine2"]);
                         }
